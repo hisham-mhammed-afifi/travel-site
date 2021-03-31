4f8e3714edf613f323a0063712bb9241327ace51
Hisham <hisham.mohammed.afifi@gmail.com>
2021-04-01 01:31:57 +0200

    completed testimonials section

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,17 @@ module.exports = {
   entry: "./app/assets/scripts/App.js",
   output: {
     filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
+    path: path.resolve(__dirname, './app')
   },
   devServer: {
-    before: function (app, server) {
+    before: function (app, server, compiler) {
       server._watch('./app/**/*.html')
     },
-    contentBase: path.join(__dirname, "app"),
+    contentBase: [
+      path.join(__dirname, './app'),
+      path.join(__dirname, './assets')
+    ],
+    publicPath: '/',
     hot: true,
     port: 3000,
     host: '0.0.0.0'
@@ -29,8 +33,15 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [
-          'style-loader',
-          'css-loader',
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            }
+          },
           {
           loader: 'postcss-loader',
             options:

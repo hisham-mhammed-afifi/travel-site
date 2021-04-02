c60469123183bd04acdeac6ac6f5748692303175
Hisham <hisham.mohammed.afifi@gmail.com>
2021-04-02 23:12:16 +0200

    completed the modal section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,10 +5,27 @@ import StickyHeader from './modules/StickyHeader'
 
 
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"));
 new RevealOnScroll(document.querySelectorAll(".testimonials"));
-let stickyHeader = new StickyHeader();
+new StickyHeader();
+let modal
+
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "Modal" */"./modules/Modal").then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("problem here"))
+
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 
 if (module.hot) {

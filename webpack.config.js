a1f447730bcaa5e30eac6465f0ca048571c9ccd9
Hisham <hisham.mohammed.afifi@gmail.com>
2021-04-03 03:41:25 +0200

    completed build process configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
+const { Compiler } = require('webpack')
+
 
 const postCSSPlugins = [
   require('postcss-import'),
@@ -8,34 +15,20 @@ const postCSSPlugins = [
   require('autoprefixer')
 ]
 
-module.exports = {
-  entry: "./app/assets/scripts/App.js",
-  output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, './app')
-  },
-  devServer: {
-    before: function (app, server, compiler) {
-      server._watch('./app/**/*.html')
-    },
-    contentBase: [
-      path.join(__dirname, './app'),
-      path.join(__dirname, './assets')
-    ],
-    publicPath: '/',
-    hot: true,
-    port: 3000,
-    host: '0.0.0.0'
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
+
+class RunAfterCompile {
+  apply(Compiler) {
+  Compiler.hooks.done.tap('copy images', function () {
+      fse.copySync('./app/assets/images', './docs/assets/images')
+    })
+  }
+}
+
+
+
+let cssConfig = {
         test: /\.css$/i,
         use: [
-          {
-            loader: 'style-loader',
-          },
           {
             loader: 'css-loader',
             options: {
@@ -54,6 +47,76 @@ module.exports = {
           }
         ]
       }
+
+
+let pages = fse.readdirSync('./app').filter(function (file) {
+  return file.endsWith('.html')
+}).map(function (page) {
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`
+  })
+})
+
+let config = {
+  entry: "./app/assets/scripts/App.js",
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig
     ]
   }
 }
+
+
+// --------for development
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+
+  config.output = {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, './app')
+  }
+  config.devServer = {
+    before: function (app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: [
+      path.join(__dirname, './app'),
+      path.join(__dirname, './assets')
+    ],
+    publicPath: '/',
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+  config.mode = 'development'
+}
+
+
+// ----------for production
+
+
+if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssPlugin.loader)
+
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, './docs')
+  }
+  config.mode = 'production'
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssPlugin({
+      filename: 'styles.[chunkhash].css'
+    }),
+    new RunAfterCompile()
+  )
+}
+
+
+module.exports = config
+
+
